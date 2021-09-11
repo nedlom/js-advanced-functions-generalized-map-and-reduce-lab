8cd43cf1c31bd3db6d4c7da3bb42ead48d735d3a
Nick <nelom3@gmail.com>
2021-09-10 20:10:29 -0700

    commit tree 3b13ee91fc194171bf3c88df7b9f4d7dcc700e19

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+function map(a, f) {
+  const b = []
+
+  for(const c in a) {
+     b.push(f(a[c]));
+  }
+
+  return b;
+};
+
+
+function reduce(src, callback, startingValue) {
+  let total;
+  if (startingValue) {
+    total = startingValue;
+    for (let i = 0; i < src.length; i++) {
+      total = callback(src[i], total);
+    }
+    return total;
+  } else {
+    total = src[0];
+    for (let i = 1; i < src.length; i++) {
+      total = callback(src[i], total);
+    }
+    return total;
+  }
+}
